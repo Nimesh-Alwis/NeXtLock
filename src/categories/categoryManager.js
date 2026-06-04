8f47ee969869906ea5d8f67a3caf3125ede27292
nimesh alwis <nimeshalwisoff@gmail.com>
2026-06-04 12:13:12 +0530

    Updated category manager structure with names

--- src/categories/categoryManager.js
+++ src/categories/categoryManager.js
@@ -1,17 +1,21 @@
 // Category Manager
 
+// Create a new category
 function createCategory(categoryName) {
 
 }
 
+// Rename an existing category
 function renameCategory(oldName, newName) {
 
 }
 
+// Delete a category
 function deleteCategory(categoryName) {
 
 }
 
+// Load all categories
 function loadCategories() {
 
 }
